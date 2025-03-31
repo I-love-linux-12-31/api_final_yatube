c5f83ae16e7b1737f3c8be459c5209dd07a26744
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2025-03-31 16:27:12 +0300

    Added permission class for comment view set

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,7 +1,20 @@
 from rest_framework import permissions
+import django.contrib.auth.models
+
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        return request.user.is_authenticated and obj.author == request.user
+
+class IsAuthorOrReadOnlyForComments(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if not request.user.is_authenticated:
+            return False
+        if isinstance(request.user, django.contrib.auth.models.AnonymousUser):
+            # Not working ?!!
+            return False
         return obj.author == request.user
